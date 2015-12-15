5a24202abfe0ea485fb252edce66a20e7311add5
Edgar Duran <edgarduran@Edgars-MacBook-Pro-6.local>
2015-12-14 21:31:18 -0700

    set up testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,18 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+require 'mocha/mini_test'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  class ActionDispatch::IntegrationTest
+    include Capybara::DSL
+
+    def teardown
+      reset_session!
+    end
+  end
 end
