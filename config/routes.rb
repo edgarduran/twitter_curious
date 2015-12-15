dd3a7414e1b6f1e81e7856e18da15e29c363d47d
Edgar Duran <edgarduran@Edgars-MacBook-Pro-6.local>
2015-12-14 22:27:39 -0700

    fix route issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get    '/',       to: 'home#home'
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+
+  get '/',             to: 'home#home'
+  get '/auth/twitter', as: :login
+  get '/logout',       to: 'sessions#destroy'
 
 end
